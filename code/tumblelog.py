0990721df90520cddbf5e952a7ae158b5d2ec858
RISMME <python@istruly.sexy>
2018-05-04 23:53:59 +0900

    UPDATE: python2.7 -> python3

--- code/tumblelog.py
+++ code/tumblelog.py
@@ -2,14 +2,17 @@ from mongoengine import *
 
 connect('tumblelog')
 
+
 class Comment(EmbeddedDocument):
     content = StringField()
     name = StringField(max_length=120)
+
 
 class User(Document):
     email = StringField(required=True)
     first_name = StringField(max_length=50)
     last_name = StringField(max_length=50)
+
 
 class Post(Document):
     title = StringField(max_length=120, required=True)
@@ -24,8 +27,10 @@ class Post(Document):
 class TextPost(Post):
     content = StringField()
 
+
 class ImagePost(Post):
     image_path = StringField()
+
 
 class LinkPost(Post):
     link_url = StringField()
@@ -45,27 +50,27 @@ post2.link_url = 'http://tractiondigital.com/labs/mongoengine/docs'
 post2.tags = ['mongoengine']
 post2.save()
 
-print 'ALL POSTS'
-print
+print('ALL POSTS')
+print()
 for post in Post.objects:
-    print post.title
-    #print '=' * post.title.count()
-    print "=" * 20
+    print(post.title)
+    # print '=' * post.title.count()
+    print("=" * 20)
 
     if isinstance(post, TextPost):
-        print post.content
+        print(post.content)
 
     if isinstance(post, LinkPost):
-        print 'Link:', post.link_url
+        print('Link:', post.link_url)
 
-    print
-print
+    print()
+print()
 
-print 'POSTS TAGGED \'MONGODB\''
-print
+print('POSTS TAGGED \'MONGODB\'')
+print()
 for post in Post.objects(tags='mongodb'):
-    print post.title
-print
+    print(post.title)
+print()
 
 num_posts = Post.objects(tags='mongodb').count()
-print 'Found %d posts with tag "mongodb"' % num_posts
+print('Found %d posts with tag "mongodb"' % num_posts)
